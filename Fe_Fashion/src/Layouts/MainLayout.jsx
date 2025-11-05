c3a28f4a3ea6210562acabd69b032ba7ac616918
Tunphun04 <phamtuanphuong2101@gmail.com>
2025-11-05 13:03:42 +0700

    continue header...

--- Fe_Fashion/src/Layouts/MainLayout.jsx
+++ Fe_Fashion/src/Layouts/MainLayout.jsx
@@ -1,37 +1,84 @@
-
+import SearchView from "../components/searchView";
+import { useState, useRef, useEffect } from "react"
 export default function MainLayout(){
+    const [isSearching, setIsSearching] = useState(false);
+    const containerRef = useRef(null); // chứa input + search view
+
+ // Click outside để đóng search view
+    useEffect(() => {
+        function handleDocClick(e) {
+        if (!containerRef.current) return;
+        if (!containerRef.current.contains(e.target)) {
+            setIsSearching(false);
+        }
+        }
+
+        function handleEsc(e) {
+        if (e.key === "Escape") setIsSearching(false);
+        }
+
+        document.addEventListener("mousedown", handleDocClick);
+        document.addEventListener("keydown", handleEsc);
+        return () => {
+        document.removeEventListener("mousedown", handleDocClick);
+        document.removeEventListener("keydown", handleEsc);
+        };
+    }, []);
+    
     return(
         <main className="flex flex-col">
-            <nav className="flex justify-between items-center align-middle h-18 font-bold text-lg font-serif ">
-                <div className="flex ml-10 gap-x-5">
+            <nav className={`flex justify-between items-center h-18 font-bold text-base font-serif transition-all duration-500
+                `}>
+                <div className={`flex ml-20 gap-x-5 transition-all duration-500 ease-in-out transform ${
+                    isSearching
+                        ? "opacity-0 -translate-y-5 pointer-events-none"
+                        : "opacity-100 translate-y-0"
+                    }`}>
                     <a href="">Men</a>
                     <a href="">Women</a>
                     <a href="">Kid</a>
                  </div>
-                <div className="">
+                <div className="text-4xl">
                         Logo
                 </div>
-                <div className="mr-10 flex gap-x-5">
+                <div className={`mr-25 flex gap-x-5 
+                    transition-all duration-500 ease-in-out transform ${
+                    isSearching
+                        ? "opacity-0 -translate-y-5 pointer-events-none"
+                        : "opacity-100 translate-y-0"
+                    }`}>
                     <a href="">Cart</a>
-                    <a href="">Favourite</a>
+                    <a href="">Wishlist</a>
                     <a href="">Profile</a>
                 </div>
             </nav>
-            <nav className="flex justify-between shadow-sm font-serif">
-                <div className=" ml-10 gap-x-5 " >
-                    <ul className="flex gap-x-5 h-10 items-center ">
+            <nav className="flex  shadow-sm font-serif">
+                 <div className={`${isSearching ?"ml-0 flex-1" :"ml-20 gap-x-5 flex min-w-[300px] flex-1 "} `}>
+                    <div className={`${isSearching ? "hidden" : "block"}`}>
+                        <ul className="flex gap-x-5 h-10 items-center text-sm">
                         <li><a href="">Clothing</a></li>
                         <li><a href="">Shoes</a></li>
                         <li><a href="">Bags</a></li>
                         <li><a href="">Watches</a></li>
                         <li><a href="">Jewelry</a></li>
                         <li><a href="">Brands</a></li>
-                    </ul>
+                        </ul>
+                    </div>
+                    <div className={`w-full ${isSearching ? "block" : "hidden"}`}>
+                        <SearchView isSearching={isSearching} />
+                    </div>
+
+                 </div>
+                <div className={`mr-20 flex items-center ${isSearching ? "mr-20 flex items-start" : "mr-20 flex items-center"} `}ref={containerRef}>
+                    <input id='search' type="search" placeholder="What are you looking for?" 
+                    className={` focus:outline-none border-b-1 pl-5 mb-1
+                    max-h-40 focus:border-b-2 ${isSearching ? "w-100 transition-all ease-in-out transform" :"w-60"}
+                    `}
+                    onFocus={() => setIsSearching(true)}
+                    />
                 </div>
-                <div className="mr-10">
-                    <input type="search" placeholder="What are you looking for?" 
-                    className="w-60 focus:outline-none border-b-1 pl-5" />
-                </div>
+                
+                
             </nav>
            
         </main>
---
+++ Fe_Fashion/src/components/searchView.jsx
@@ -0,0 +1,16 @@
+export default function SearchView({isSearching}){
+        return(
+        <div className={`transition-all duration-500 ease-in-out transform ${
+          isSearching
+            ? "opacity-100 translate-y-0 h-100"
+            : "opacity-0 -translate-y-3 pointer-events-none"
+        }`}>
+            <header className="flex ml-20 gap-x-5 transition-all ease-in-out transform
+            h-auto font-bold text-base font-serif border-b-1 mr-20">
+                <a href="">Men</a>
+                <a href="">Women</a>
+                 <a href="">Kid</a>
+            </header>
+        </div>
+        )
+    }
